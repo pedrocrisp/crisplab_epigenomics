a6bf711f0b40bcec7aea486bae2068d99355b46a
Peter Crisp <pedrocrisp@gmail.com>
2020-09-30 13:15:00 +1000

    path typo

--- RNAseq/01-qc_qsub.sh
+++ RNAseq/01-qc_qsub.sh
@@ -49,7 +49,7 @@ log_folder=logs/${timestamp}_${step}
 mkdir $log_folder
 
 #script path and cat a record of what was run
-script_dir=~/gitrepos/crisplab_epigenomics/ATACseq
+script_dir=~/gitrepos/crisplab_epigenomics/RNAseq
 script_to_qsub=${script_dir}/${step}.sh
 cat $script_to_qsub > ${log_folder}/script.log
 cat $0 > ${log_folder}/qsub_runner.log
