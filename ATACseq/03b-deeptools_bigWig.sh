444eca0a480fc41a68779a891c4ed5ed4246692f
Peter Crisp <pedrocrisp@gmail.com>
2020-11-30 23:59:35 +1000

    normalised bigwigs

---
+++ ATACseq/03b-deeptools_bigWig.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -l
+#PBS -A UQ-SCI-SAFS
+#PBS -N bigWigs_tiles
+#PBS -r y
+#PBS -m abej
+#PBS -M p.crisp@uq.edu.au
+
+########## QC #################
+set -xeuo pipefail
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo PBS: array_ID is ${PBS_ARRAY_INDEX}
+echo ------------------------------------------------------
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to PBS_O_WORKDIR
+cd "$PBS_O_WORKDIR"
+echo working dir is now $PWD
+
+########## Modules #################
+conda activate py3.7
+
+########## Set up dirs #################
+
+#get job ID
+#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAY_INDEX} eg 2 from {list}
+ID="$(/bin/sed -n ${PBS_ARRAY_INDEX}p ${LIST})"
+
+echo sample being mapped is $ID
+
+#make adaligned folder bsmaped
+cd analysis
+mkdir -p bigWigs_deeptools
+
+########## Run #################
+# make normalised bigWigs
+# ising --extendReads as suggested for contiguous mapping data like ChIP to map whole of PE frag
+bamCoverage \
+--bam $bam_dir/${ID}_sorted.bam \
+-o bigWigs_deeptools/${ID}.bw \
+--binSize 10 \
+--normalizeUsing CPM \
+--extendReads \
+-p 2
+
+echo finished summarising
