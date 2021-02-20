1afc958436611dd49956eb1df1a00a020e6d9b36
Peter Crisp <pedrocrisp@gmail.com>
2021-02-20 20:50:14 +1000

    11b

---
+++ methylome/11b-DSS_call_tile_DMRs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -l
+#PBS -A UQ-SCI-SAFS
+#PBS -N DSS_DMRs
+#PBS -r y
+#PBS -m abej
+#PBS -M p.crisp@uq.edu.au
+
+########## QC #################
+set -xeuo pipefail
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo PBS: array_ID is ${PBS_ARRAY_INDEX}
+echo ------------------------------------------------------
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to PBS_O_WORKDIR
+cd "$PBS_O_WORKDIR"
+echo working dir is now $PWD
+
+########## Modules #################
+
+module load R/3.5.0-gnu
+########## Set up dirs #################
+
+#get job ID
+#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAY_INDEX} eg 2 from {list}
+ID="$(/bin/sed -n ${PBS_ARRAY_INDEX}p ${LIST})"
+
+echo contrast is $ID
+echo data folder is $path_to_data_files
+
+########## Run #################
+
+        #Run R moudle to:
+        R -f ~/gitrepos/crisplab_epigenomics/methylome/11b-DSS_call_tile_DMRs.R \
+        --args ${ID} $DMR_contrasts_table_file $path_to_data_files
+
+echo finished summarising
