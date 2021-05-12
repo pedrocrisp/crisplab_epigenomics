e7c219e5b884d43ba75885d0476b19d3c0bced3f
Peter Crisp <pedrocrisp@gmail.com>
2021-05-12 23:05:02 +1000

    filter merge

---
+++ methylome/04b-filterd-merge-splits.sh
@@ -0,0 +1,123 @@
+#!/bin/bash -l
+#PBS -A UQ-SCI-SAFS
+#PBS -N merge
+#PBS -r y
+#PBS -m abej
+#PBS -M p.crisp@uq.edu.au
+
+########## QC #################
+set -xeuo pipefail
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo PBS: array_ID is ${PBS_ARRAY_INDEX}
+echo ------------------------------------------------------
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to PBS_O_WORKDIR
+cd "$PBS_O_WORKDIR"
+echo working dir is now $PWD
+
+########## Modules #################
+module load samtools/1.9
+
+########## Set up dirs #################
+
+#get job ID
+#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAY_INDEX} eg 2 from {list}
+ID="$(/bin/sed -n ${PBS_ARRAY_INDEX}p ${LIST})"
+
+echo sample being mapped is $ID
+
+cd analysis
+
+# make sure you already created a folder called bsmapped_filtered_merge and copied the bams over
+# eg
+# rsync -rhivPt bsmapped_filtered/*pairs_clipOverlap.bam bsmapped_filtered_merge/
+
+if [ "$paired_end" == "yes" ]
+then
+
+########## Run #################
+
+# merge
+samtools merge \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap.bam \
+bsmapped_filtered_merge/${ID}_${split_1_name}_sorted_MarkDup_pairs_clipOverlap.bam \
+bsmapped_filtered_merge/${ID}_${split_2_name}_sorted_MarkDup_pairs_clipOverlap.bam
+
+# grab header for later
+samtools view -H bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap.bam > \
+bsmapped_filtered_merge/${ID}_${split_2_name}_header.sam
+
+# view to sam (can merge output a sam? - yes)
+samtools view bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap.bam \
+-o bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap.sam
+
+# Add NH flag
+# This took around 60Gb and an hour
+awk -v FS='\t' -v OFS='\t' 'NR == FNR {T[$1]++; next} {print $0, "NH:i:"T[$1]}' \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap.sam > \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH.sam
+
+# Count NH flag and sam flag distribution
+grep -oP 'NH:i:\d+' \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH.sam | \
+sed 's/:/\t/g' | cut -f3 | sort | uniq -c > \
+bsmapped_filtered_merge/${ID}_merged_NH_distribution.txt
+
+# sam flag distribution
+# This takes a couple if gig and 20 min or so
+cut -f 2 bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH.sam | \
+sort | uniq -c > \
+bsmapped_filtered_merge/${ID}_merged_sam_flag_distribution.txt
+
+# Filter to reads (QNAME) occurring twice in sam
+# Because these are properly paired reads and QNAME marks reads from the same
+# molecule/fragment PE reads have the same QNAME. I have previously filtered to
+# mapped and properly paired so filtering noe to NH == 2 should get us what we want...
+grep -w 'NH:i:2' \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH.sam > \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH_unique.sam
+
+# Add header, sort compress
+cat bsmapped_filtered_merge/${ID}_${split_2_name}_header.sam \
+bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH_unique.sam | \
+samtools sort \
+-O bam \
+-@ 2 \
+-m 2G \
+-o bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH_unique_sorted.bam
+
+# Put his file back in the bsmapped folder
+# Use original sample name and giv suffix consistent with the pipeline.
+mv -rv bsmapped_filtered_merge/${ID}_merged_sorted_MarkDup_pairs_clipOverlap_NH_unique_sorted.bam \
+bsmapped_filtered/${ID}_sorted_MarkDup_pairs_clipOverlap.bam
+
+# once this completes successfully delete all bams/sams in the merge folder (or uncomment this)
+# rm -rv bsmapped_filtered_merge/*bam
+# rm -rv bsmapped_filtered_merge/*sam
+
+elif [ "$paired_end" == "no" ]
+then
+
+echo "uh oh - havent thought about how to deal with SE yet... better write some more code"
+
+else
+
+echo "library type not specified correctly, please indicate yes for PE or no for SE"
+
+fi
---
+++ methylome/04b-filterd-merge-splits_qsub.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#set -xe
+set -xeuo pipefail
+
+usage="USAGE:
+bash 04b-filterd-merge-splits_qsub.sh <sample_list.txt> <paired_end> <split_1_name> <split_2_name> <walltime> <memory>
+for example:
+bash \
+/home/springer/pcrisp/gitrepos/springerlab_methylation/SeqCap/04b-filterd-merge-splits_qsub.sh \
+single_sample.txt \
+yes \
+24:00:00 \
+chr1-4 \
+chr5-7 \
+40
+"
+
+#define stepo in the pipeline - should be the same name as the script
+step=04b-filterd-merge-splits
+
+######### Setup ################
+sample_list=$1
+paired_end=$2
+split_1_name=$3
+split_2_name=$4
+walltime=$5
+mem=$6
+
+if [ "$#" -lt "6" ]
+then
+echo $usage
+exit -1
+else
+echo "Submitting samples listed in '$sample_list' for merging"
+cat $sample_list
+fi
+
+#number of samples
+number_of_samples=`wc -l $sample_list | awk '{print $1}'`
+if [[ "$number_of_samples" -eq 1 ]]
+then
+qsub_t=1
+else
+qsub_t="1-${number_of_samples}"
+fi
+echo "argument to be passed to qsub -t is '$qsub_t'"
+
+#find script to run, makes it file system agnostic
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+
+########## Run #################
+
+#make log and analysis folders
+#make logs folder if it doesnt exist yet
+mkdir -p logs
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+#make analysis dir if it doesnt exist yet
+analysis_dir=analysis
+mkdir -p $analysis_dir
+
+#make trimmgalore logs folder, timestamped
+log_folder=logs/${timestamp}_${step}
+mkdir $log_folder
+
+#script path and cat a record of what was run
+script_to_qsub=${scriptdir}/${step}.sh
+cat $script_to_qsub > ${log_folder}/script.log
+cat $0 > ${log_folder}/qsub_runner.log
+
+#submit qsub and pass args
+#-o and -e pass the file locations for std out/error
+#-v additional variables to pass to the qsub script including the PBS_array list and the dir structures
+qsub -J $qsub_t \
+-l walltime=${walltime},nodes=1:ppn=2,mem=${mem}gb \
+-o ${log_folder}/${step}_o^array_index^ \
+-e ${log_folder}/${step}_e^array_index^ \
+-v LIST=${sample_list},paired_end=$paired_end,split_1_name=$split_1_name,split_2_name=$split_2_name \
+$script_to_qsub
