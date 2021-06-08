a97281906a29278c17463bd96127b7bb4dc818d3
Peter Crisp <pedrocrisp@gmail.com>
2021-06-08 23:26:33 +1000

    -bs 100

--- methylome/08-cov_multi_feature.sh
+++ methylome/08-cov_multi_feature.sh
@@ -60,6 +60,7 @@ computeMatrix scale-regions \
 -S $CG_bigWig \
 -b 2000 -a 2000 \
 -p 2 \
+-bs 100 \
 --regionBodyLength 300 \
 --sortRegions descend \
 -o ${out_dir}/${ID}_CG.mat.gz \
@@ -87,6 +88,7 @@ computeMatrix scale-regions \
 -S $CHG_bigWig \
 -b 2000 -a 2000 \
 -p 2 \
+-bs 100 \
 --regionBodyLength 300 \
 --sortRegions descend \
 -o ${out_dir}/${ID}_CHG.mat.gz \
@@ -114,6 +116,7 @@ computeMatrix scale-regions \
 -S $CHH_bigWig \
 -b 2000 -a 2000 \
 -p 2 \
+-bs 100 \
 --regionBodyLength 300 \
 --sortRegions descend \
 -o ${out_dir}/${ID}_CHH.mat.gz \
