4818e54bac3156ef6ddf3e4d6a966b39560676e2
Peter Crisp <pedrocrisp@gmail.com>
2021-08-11 22:27:43 +1000

    bigWigs_outdir

--- RNAseq/04-bigwigs.sh
+++ RNAseq/04-bigwigs.sh
@@ -45,7 +45,7 @@ sample_dir=$alignFolder
 outdir=analysis/bigWig_intermediate_${alignFolder}
 mkdir -p ${outdir}
 
-bigWigs_outdir=analysis/bigWigs_${alignFolder}
+bigWigs_outdir=analysis/${alignFolder}_bigWigs
 mkdir -p ${bigWigs_outdir}
 
 # Condition statement: if library is stranded $3 == stranded and bigWigs are made for each strand.  If library is nonstranded $3 == nonstranded and nonstrandspecific bigWig is made.  If no strand info is specified, script will error
