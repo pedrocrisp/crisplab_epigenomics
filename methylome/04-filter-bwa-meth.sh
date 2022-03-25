91a0589f82815029cbe3b049c19198d1b4772659
Peter Crisp <pedrocrisp@gmail.com>
2022-03-25 20:34:25 +1000

    bwa-meth-filter module

---
+++ methylome/04-filter-bwa-meth.sh
@@ -0,0 +1,131 @@
+#!/bin/bash -l
+#PBS -A UQ-SCI-SAFS
+#PBS -N filter-bwa-meth
+#PBS -r y
+#PBS -m abej
+#PBS -M p.crisp@uq.edu.au
+
+########## QC #################
+set -xeuo pipefail
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo PBS: array_ID is ${PBS_ARRAY_INDEX}
+echo ------------------------------------------------------
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to PBS_O_WORKDIR
+cd "$PBS_O_WORKDIR"
+echo working dir is now $PWD
+
+########## Modules #################
+# requires java 1.8
+module load Java/1.8.0_45
+#module load bedtools
+module load bamtools
+#bsmap requires samtools < 1.0.0
+#PATH=~/software/bsmap-2.74/samtools:$PATH
+module load samtools
+
+########## Set up dirs #################
+
+#get job ID
+#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAY_INDEX} eg 2 from {list}
+ID="$(/bin/sed -n ${PBS_ARRAY_INDEX}p ${LIST})"
+
+echo sample being mapped is $ID
+
+cd analysis
+mkdir -p bwa-meth-filtered
+
+if [ "$paired_end" == "yes" ]
+then
+
+########## Run #################
+
+        # fix improperly paird reads - specifically discordant read pairs that are incorrectly mraked as concordant by bsmap
+        # picard FixMateInformation
+        # didnt work...
+        #java -jar /home/springer/pcrisp/software/picard.jar FixMateInformation \
+        #I=bsmapped/${ID}.bam \
+        #O=bwa-meth-filtered/${ID}_sorted.bam
+
+        #remove PCR duplicates, must be sorted by coordinate using pickard
+
+        # bams already co-ordinate sorted by samtools, this step seems unnecessary
+        # Also causesing issues: bsmap is reporting PE reads as properly mapped where they hit different chromosomes, solution: skip step
+        # uncomment to re-instate
+
+        #java -jar /home/springer/pcrisp/software/picard.jar SortSam \
+        #INPUT=bsmapped/${ID}.bam \
+        #OUTPUT=bsmapped/${ID}_sorted.bam \
+        #SORT_ORDER=coordinate
+
+        #mark duplicates
+        #requires sorted input - using samtools sort in bsmap step (co-ordinate sorted)
+        # if co-ordinate sorted then pairs where the mate is unmapped or has secondary alignment are not marked as duplicate
+        # ASSUME_SORTED=true because sorting performed with samtools but samtools doesnt seem to add this flag to the headder
+        java -jar ~/software/picard.jar MarkDuplicates \
+        I=bwa-meth/${ID}_sorted.bam \
+        O=bwa-meth-filtered/${ID}_sorted_MarkDup.bam \
+        METRICS_FILE=bwa-meth-filtered/${ID}_MarkDupMetrics.txt \
+        ASSUME_SORTED=true \
+        CREATE_INDEX=False \
+        REMOVE_DUPLICATES=true
+
+        # keep properly paired reads using bamtools package
+        # note that some reads marked as properly paired by bsmap actually map to different chromosomes
+        bamtools filter \
+        -isMapped true \
+        -isPaired true \
+        -isProperPair true \
+        -in bwa-meth-filtered/${ID}_sorted_MarkDup.bam \
+        -out bwa-meth-filtered/${ID}_sorted_MarkDup_pairs.bam
+
+        # clip overlapping reads using bamUtils package
+        bam clipOverlap \
+        --in bwa-meth-filtered/${ID}_sorted_MarkDup_pairs.bam \
+        --out bwa-meth-filtered/${ID}_sorted_MarkDup_pairs_clipOverlap.bam \
+        --stats
+
+        #index bam
+        # index
+        samtools index bwa-meth-filtered/${ID}_sorted_MarkDup_pairs_clipOverlap.bam
+
+elif [ "$paired_end" == "no" ]
+then
+
+# remove duplicate
+java -jar ~/software/picard.jar MarkDuplicates \
+I=bwa-meth/${ID}_sorted.bam \
+O=bwa-meth-filtered/${ID}_sorted_MarkDup.bam \
+METRICS_FILE=bwa-meth-filtered/${ID}_MarkDupMetrics.txt \
+ASSUME_SORTED=true \
+CREATE_INDEX=False \
+REMOVE_DUPLICATES=true
+
+# rename to keep script naming convention consistent
+mv bwa-meth-filtered/${ID}_sorted_MarkDup.bam bwa-meth-filtered/${ID}_sorted_MarkDup_pairs_clipOverlap.bam
+
+# index
+samtools index bwa-meth-filtered/${ID}_sorted_MarkDup_pairs_clipOverlap.bam
+
+
+else
+
+echo "library type not specified correctly, please indicate yes for PE or no for SE"
+
+fi
