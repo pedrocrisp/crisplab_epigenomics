836a309d991846892407ca71b351f2a81dbe3f57
Peter Crisp <pedrocrisp@gmail.com>
2022-04-15 01:56:23 +1000

    bigwig?

--- methylome/05-summarise_MethylDackel.sh
+++ methylome/05-summarise_MethylDackel.sh
@@ -106,15 +106,15 @@ tail -n+2 MethylDackel/${ID}_methratio_head_CHH.bedGraph > MethylDackel/${ID}_me
 # Add context - uncomment if you want to add column with context
 awk -F$"\\t" \
 'BEGIN {OFS = FS} (NR>1){print $1, $2, $3, $4}' \
- MethylDackel/${ID}_methratio_head_CpG.bedGraph > MethylDackel/${ID}_methratio_CG.bedGraph
+MethylDackel/${ID}_methratio_head_CpG.bedGraph > MethylDackel/${ID}_methratio_CG.bedGraph
 
- awk -F$"\\t" \
- 'BEGIN {OFS = FS} (NR>1){print $1, $2, $3, $4}' \
-  MethylDackel/${ID}_methratio_head_CHG.bedGraph > MethylDackel/${ID}_methratio_CHG.bedGraph
+awk -F$"\\t" \
+'BEGIN {OFS = FS} (NR>1){print $1, $2, $3, $4}' \
+MethylDackel/${ID}_methratio_head_CHG.bedGraph > MethylDackel/${ID}_methratio_CHG.bedGraph
 
-  awk -F$"\\t" \
-  'BEGIN {OFS = FS} (NR>1){print $1, $2, $3, $4}' \
-   MethylDackel/${ID}_methratio_head_CHH.bedGraph > MethylDackel/${ID}_methratio_CHH.bedGraph
+awk -F$"\\t" \
+'BEGIN {OFS = FS} (NR>1){print $1, $2, $3, $4}' \
+ MethylDackel/${ID}_methratio_head_CHH.bedGraph > MethylDackel/${ID}_methratio_CHH.bedGraph
 
 # rm head files
 rm -rv MethylDackel/${ID}_methratio_head*
