52cf3b33368d94155cdd17b8616986a7f09fcc3f
Peter Crisp <pedrocrisp@gmail.com>
2022-10-28 23:14:35 +1000

    correct outfolder

--- ATACseq/07-csaw.sh
+++ ATACseq/07-csaw.sh
@@ -73,6 +73,7 @@ projectFolder=${path_to_data_files}_CSAW
 DE_UMR_bed_folder=${ID}_FCF_${filter_FC}_bin${bin_size}_space${window_spacing}
 DE_UMR_bed_file=${ID}_FCF_${filter_FC}_bin${bin_size}_space${window_spacing}_differential_UMRs_CSAW_sig_metadata.bed
 
+outFolder=${projectFolder}/${DE_UMR_bed_folder}/annotation
 # gene annotation
 
 bedtools closest \
