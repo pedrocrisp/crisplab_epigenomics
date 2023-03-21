f0392129d69e588597039d076f796daed00bc370
Pete <pedrocrisp@users.noreply.github.com>
2023-03-21 18:32:36 +1000

    SLURM make tiles

---
+++ methylome-s/create_genome_tile_mC_counts.sh
@@ -0,0 +1,134 @@
+#!/bin/bash -l
+#SBATCH --job-name fastqc
+#SBATCH --requeue
+#SBATCH --partition=general
+
+########## QC #################
+set -xeuo pipefail
+
+echo ------------------------------------------------------
+echo SBATCH: working directory is $SLURM_SUBMIT_DIR
+echo SBATCH: job identifier is $SLURM_JOBID
+echo SBATCH: array_ID is ${SLURM_ARRAY_TASK_ID}
+echo ------------------------------------------------------
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to SLURM_SUBMIT_DIR
+cd "$SLURM_SUBMIT_DIR"
+echo working dir is now $PWD
+
+########## Modules #################
+
+module load r/4.2.1-foss-2021a
+########## Set up dirs #################
+
+#get job ID
+#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAY_INDEX} eg 2 from {list}
+ID="$(/bin/sed -n ${SLURM_ARRAY_TASK_ID}p ${LIST})"
+
+echo sample being mapped is $ID
+
+genome=$ID
+echo genome is $genome
+
+mkdir -p sites
+cd sites
+
+########## Run MODULE 1 #################
+
+# Run R module to creat 100bp tile bed file
+R -f ~/gitrepos/crisplab_epigenomics/methylome/create_genome_tile_mC_counts_tiles.R \
+--args $genome
+
+##########              #################
+########## Run MODULE 2 #################
+##########              #################
+# Run module to count number of CG/CHG/CHH sites in Each 100bp tile
+
+module load bedtools
+
+bedtools getfasta -fi ../${genome}.fa \
+-bed ${genome}_100pb_tiles_for_sites_calc.bed \
+-fo ${genome}_100bp_tiles.fa
+
+# these fasta records contain the 100 nt tile plus 2 nt either side for classifying sites bridging the tiles
+
+####### counts sites
+# make a fasta file of the motifs to search for
+printf ">CG\nCG\n>CHG\nC[ATC]G\n>CHH\nC[ACT][ACT]" > contexts_motifs.fa
+
+#cat contexts_motifs.fa
+#>CG
+#CG
+#>CHG
+#C[ATC]G
+#>CHH
+#C[ACT][ACT]
+
+######### find motifs ##########
+
+# seqkit locate finds the location of all CG/CHG and CHH sites
+# awk removes any C sites identified in the first or last 2 bp flanks of each tile
+# csvtk summarises per tile per context
+
+time \
+cat ${genome}_100bp_tiles.fa | \
+seqkit locate -i -r -f contexts_motifs.fa | \
+awk '(NR==1) ||
+($1 ~ /:0-102/) && ($2 == "CG") && ($4 == "+") && ($5 >= 1) && ($6 <= 101) ||
+($1 ~ /:0-102/) && ($2 == "CG") && ($4 == "-") && ($5 >= 1) && ($6 <= 100)  ||
+($1 ~ /:0-102/) && ($2 == "CHG") && ($4 == "+") && ($5 >= 1) && ($6 <= 102) ||
+($1 ~ /:0-102/) && ($2 == "CHG") && ($4 == "-") && ($5 >= 1) && ($6 <= 100)  ||
+($1 ~ /:0-102/) && ($2 == "CHH") && ($4 == "+") && ($5 >= 1) && ($6 <= 104) ||
+($1 ~ /:0-102/) && ($2 == "CHH") && ($4 == "-") && ($5 >= 1) && ($6 <= 100) ||
+($1 !~ /:0-102/) && ($2 == "CG") && ($4 == "+") && ($5 >= 3) && ($6 <= 103) ||
+($1 !~ /:0-102/) && ($2 == "CG") && ($4 == "-") && ($5 >= 2) && ($6 <= 102)  ||
+($1 !~ /:0-102/) && ($2 == "CHG") && ($4 == "+") && ($5 >= 3) && ($6 <= 104) ||
+($1 !~ /:0-102/) && ($2 == "CHG") && ($4 == "-") && ($5 >= 1) && ($6 <= 102)  ||
+($1 !~ /:0-102/) && ($2 == "CHH") && ($4 == "+") && ($5 >= 3) && ($6 <= 104) ||
+($1 !~ /:0-102/) && ($2 == "CHH") && ($4 == "-") && ($5 >= 1) && ($6 <= 102) ' | \
+csvtk -t freq -k -f 1,2 > ${genome}_100bp_tiles_sites.tsv
+
+####### counts sites inc N?
+# make a fasta file of the motifs to search for
+printf ">CG\nCG\n>CHG\nC[ATC]G\n>CHH\nC[ACT][ACT]\n>N\nN" > contexts_motifs_N.fa
+#cat contexts_motifs_N.fa
+#>CG
+#CG
+#>CHG
+#C[ATC]G
+#>CHH
+#C[ACT][ACT]
+#>N
+#N
+
+cat ${genome}_100bp_tiles.fa | \
+seqkit locate -i -r -f contexts_motifs_N.fa | \
+awk '(NR==1) ||
+($1 ~ /:0-102/) && ($2 == "CG") && ($4 == "+") && ($5 >= 1) && ($6 <= 101) ||
+($1 ~ /:0-102/) && ($2 == "CG") && ($4 == "-") && ($5 >= 1) && ($6 <= 100)  ||
+($1 ~ /:0-102/) && ($2 == "CHG") && ($4 == "+") && ($5 >= 1) && ($6 <= 102) ||
+($1 ~ /:0-102/) && ($2 == "CHG") && ($4 == "-") && ($5 >= 1) && ($6 <= 100)  ||
+($1 ~ /:0-102/) && ($2 == "CHH") && ($4 == "+") && ($5 >= 1) && ($6 <= 104) ||
+($1 ~ /:0-102/) && ($2 == "CHH") && ($4 == "-") && ($5 >= 1) && ($6 <= 100) ||
+($1 !~ /:0-102/) && ($2 == "CG") && ($4 == "+") && ($5 >= 3) && ($6 <= 103) ||
+($1 !~ /:0-102/) && ($2 == "CG") && ($4 == "-") && ($5 >= 2) && ($6 <= 102)  ||
+($1 !~ /:0-102/) && ($2 == "CHG") && ($4 == "+") && ($5 >= 3) && ($6 <= 104) ||
+($1 !~ /:0-102/) && ($2 == "CHG") && ($4 == "-") && ($5 >= 1) && ($6 <= 102)  ||
+($1 !~ /:0-102/) && ($2 == "CHH") && ($4 == "+") && ($5 >= 3) && ($6 <= 104) ||
+($1 !~ /:0-102/) && ($2 == "CHH") && ($4 == "-") && ($5 >= 1) && ($6 <= 102) ||
+($1 ~ /:0-102/) && ($2 == "N") && ($6 <= 100) ||
+($1 !~ /:0-102/) && ($2 == "N") && ($5 >= 3) && ($6 <= 102) ' | \
+csvtk -t freq -k -f 1,2 > ${genome}_100bp_tiles_sites_Ns.tsv
+
+##########              #################
+########## Run MODULE 3 #################
+##########              #################
+
+# Run R module to count sites in each 100 bp tile
+R -f ~/gitrepos/crisplab_epigenomics/methylome/create_genome_tile_mC_counts.R \
+--args $genome
+
+echo finished summarising
---
+++ methylome-s/create_genome_tile_mC_counts_sbatch.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#set -xe
+set -xeuo pipefail
+
+#Peter Crisp
+#2019-12-12
+#Bash qsub script for create_genome_tile_mC_counts.R
+
+usage="USAGE:
+create_genome_tile_mC_counts_sbatch <sample_list> <walltime> <memory>
+"
+
+#define stepo in the pipeline - should be the same name as the script
+step=create_genome_tile_mC_counts
+
+######### Setup ################
+sample_list=$1
+walltime=$2
+mem=$3
+
+if [ "$#" -lt "3" ]
+then
+echo $usage
+exit -1
+else
+echo "Submitting samples listed in '$sample_list' for analysis"
+cat $sample_list
+fi
+
+#number of samples
+number_of_samples=`wc -l $sample_list | awk '{print $1}'`
+if [[ "$number_of_samples" -eq 1 ]]
+then
+sbatch_t=1
+else
+sbatch_t="1-${number_of_samples}"
+fi
+echo "argument to be passed to sbatch -t is '$sbatch_t'"
+
+#find script to run, makes it file system agnostic
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+
+########## Run #################
+
+#make log and analysis folders
+#make logs folder if it doesnt exist yet
+mkdir -p logs
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+#make analysis dir if it doesnt exist yet
+analysis_dir=analysis
+mkdir -p $analysis_dir
+
+#make trimmgalore logs folder, timestamped
+log_folder=logs/${timestamp}_${step}
+mkdir $log_folder
+
+#script path and cat a record of what was run
+script_to_sbatch=${scriptdir}/${step}.sh
+cat $script_to_sbatch > ${log_folder}/script.log
+cat $0 > ${log_folder}/sbatch_runner.log
+
+#submit sbatch and pass args
+#-o and -e pass the file locations for std out/error
+#-v additional variables to pass to the sbatch script including the PBS_array list and the dir structures
+sbatch --array $sbatch_t \
+-t ${walltime} \
+-N 1 \
+-n 1 \
+--mem ${mem}gb \
+-o ${log_folder}/${step}_o_%A_%a \
+-e ${log_folder}/${step}_e_%A_%a \
+--export LIST=${sample_list} \
+--account $account_department \
+$script_to_sbatch
