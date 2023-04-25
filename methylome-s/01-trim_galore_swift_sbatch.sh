38180a7d3d94b38cc2bcca457b526abf2effdb1a
Pete <pedrocrisp@users.noreply.github.com>
2023-04-25 12:14:36 +1000

    script dir

--- methylome-s/01-trim_galore_swift_sbatch.sh
+++ methylome-s/01-trim_galore_swift_sbatch.sh
@@ -49,7 +49,7 @@ log_folder=logs/${timestamp}_${step}
 mkdir $log_folder
 
 #script path and cat a record of what was run
-script_dir=~/gitrepos/crisplab_epigenomics/methylome
+script_dir=~/gitrepos/crisplab_epigenomics/methylome-s
 script_to_sbatch=${script_dir}/${step}.sh
 cat $script_to_sbatch > ${log_folder}/script.log
 cat $0 > ${log_folder}/sbatch_runner.log
