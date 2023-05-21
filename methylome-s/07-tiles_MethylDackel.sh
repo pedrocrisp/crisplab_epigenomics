218dcd80a550923c9681314f669e5a10a9baf920
Pete <pedrocrisp@users.noreply.github.com>
2023-05-21 21:30:52 +1000

    tiles_MethylDackel

---
+++ methylome-s/07-tiles_MethylDackel.sh
@@ -0,0 +1,76 @@
+#!/bin/bash -l
+#SBATCH --job-name tiles_MethylDackel
+#SBATCH --requeue
+#SBATCH --partition=general
+
+########## QC #################
+set -xeuo pipefail
+
+echo ------------------------------------------------------
+echo SBATCH: working directory is $SLURM_SUBMIT_DIR
+echo SBATCH: job identifier is $SLURM_JOBID
+echo SBATCH: array_ID is ${SLURM_ARRAY_TASK_ID}
+echo ------------------------------------------------------
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to SLURM_SUBMIT_DIR
+cd "$SLURM_SUBMIT_DIR"
+echo working dir is now $PWD
+
+########## Modules #################
+module load r/4.2.1-foss-2021a
+########## Set up dirs #################
+
+#get job ID
+#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAY_INDEX} eg 2 from {list}
+ID="$(/bin/sed -n ${SLURM_ARRAY_TASK_ID}p ${LIST})"
+
+echo sample being mapped is $ID
+
+#make adaligned folder bsmaped
+cd analysis
+mkdir -p tiles
+
+########## Run #################
+
+# Run R module to:
+# 1. sumamrise into 100bp tiles
+# 2. fix chr ends in BED file
+# 3. output one-based coordinate .txt file for analysis in R
+# folder with input files hardcoded "MethylDackel"
+
+for CHROMOSOME in $(cat $chromosome_list); do
+R -f ~/gitrepos/crisplab_epigenomics/methylome/07-tiles_MethylDackel_tile_mod.R \
+--args ${ID}_${CHROMOSOME} MethylDackel ${reference_tile_file_folder}/${CHROMOSOME}_100bp_tiles_zBased_sites_counts.txt
+done
+
+#make bedGraph by sorting and removing cols 4 and 5 with awk
+
+# make bedGraph by removing cols 4 and 5 and sorting
+# cut -f1-3,6-6 ./tiles/${ID}_BSMAP_out.txt.100.CG.fixed.bed | sort -k1,1 -k2,2n > ./tiles/${ID}_BSMAP_out.txt.100.CG.fixed.sorted.bg
+# cut -f1-3,6-6 ./tiles/${ID}_BSMAP_out.txt.100.CHG.fixed.bed | sort -k1,1 -k2,2n > ./tiles/${ID}_BSMAP_out.txt.100.CHG.fixed.sorted.bg
+# cut -f1-3,6-6 ./tiles/${ID}_BSMAP_out.txt.100.CHH.fixed.bed | sort -k1,1 -k2,2n > ./tiles/${ID}_BSMAP_out.txt.100.CHH.fixed.sorted.bg
+
+#Make bigWigs
+# At some point soft code the reference, make variable in script call
+# To mkae the chrom_sizes file use seqtk compressseqtk comp Zea_mays.AGPv4.dna.toplevel.fa
+# seqtk comp Zea_mays.AGPv4.dna.toplevel.fa | cut -f 1-2 > Zea_mays.AGPv4.dna.toplevel.chrom.sizes
+
+# note - on some genomes this bigwig fails because it is not natural sorted...
+# but i generally do use these files so no worries...
+# maybe somepoint add "sort -k1,1 -k2,2n" with LC_COLLATE=C,  or bedSort and try again
+
+# bedGraphToBigWig "./tiles/${ID}_BSMAP_out.txt.100.CG.fixed.sorted.bg" ${chrom_sizes} \
+# "./tiles/${ID}_BSMAP_out.txt.100.CG.bigWig"
+# bedGraphToBigWig "./tiles/${ID}_BSMAP_out.txt.100.CHG.fixed.sorted.bg" ${chrom_sizes} \
+# "./tiles/${ID}_BSMAP_out.txt.100.CHG.bigWig"
+# bedGraphToBigWig "./tiles/${ID}_BSMAP_out.txt.100.CHH.fixed.sorted.bg" ${chrom_sizes} \
+# "./tiles/${ID}_BSMAP_out.txt.100.CHH.bigWig"
+
+# remove .bg - not required
+# meh purge it in step 09 instead
+# rm -rv tiles/${ID}*.bg
+
+echo finished summarising
---
+++ methylome-s/07-tiles_MethylDackel_sbatch.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#set -xe
+set -xeuo pipefail
+
+usage="USAGE:
+bash 07-summarise_methylation_sbatch.sh <sample_list.txt> <chromosome_list> <reference_tile_file_folder> <walltime> <memory> <cores> <account_department>
+"
+
+#define stepo in the pipeline - should be the same name as the script
+step=07-tiles_MethylDackel
+
+######### Setup ################
+sample_list=$1
+chromosome_list=$2
+reference_tile_file_folder=$3
+walltime=$4
+mem=$5
+cores=$6
+account_department=$7
+
+if [ "$#" -lt "6" ]
+then
+echo $usage
+exit -1
+else
+echo "Submitting samples listed in '$sample_list' for binning"
+cat $sample_list
+fi
+
+#number of samples
+number_of_samples=`wc -l $sample_list | awk '{print $1}'`
+if [[ "$number_of_samples" -eq 1 ]]
+then
+sbatch_t=1
+else
+sbatch_t="1-${number_of_samples}"
+fi
+echo "argument to be passed to sbatch -t is '$sbatch_t'"
+
+#find script to run, makes it file system agnostic
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+
+########## Run #################
+
+#make log and analysis folders
+#make logs folder if it doesnt exist yet
+mkdir -p logs
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+#make analysis dir if it doesnt exist yet
+analysis_dir=analysis
+mkdir -p $analysis_dir
+
+#make trimmgalore logs folder, timestamped
+log_folder=logs/${timestamp}_${step}
+mkdir $log_folder
+
+#script path and cat a record of what was run
+script_to_sbatch=${scriptdir}/${step}.sh
+cat $script_to_sbatch > ${log_folder}/script.log
+cat $0 > ${log_folder}/sbatch_runner.log
+
+#submit sbatch and pass args
+#-o and -e pass the file locations for std out/error
+#--export additional variables to pass to the sbatch script including the array list and the dir structures
+sbatch --array $sbatch_t \
+-t ${walltime} \
+-N 1 \
+-n 1 \
+--cpus-per-task ${cores} \
+--mem ${mem}gb \
+-o ${log_folder}/${step}_o_%A_%a \
+-e ${log_folder}/${step}_e_%A_%a ź
+--export LIST=${sample_list},chromosome_list=${chromosome_list},reference_tile_file_folder=${reference_tile_file_folder} \
+--account $account_department \
+$script_to_sbatch
