16bc87a8715215551c9f6f35f9cfba939d5824d7
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-06-16 10:59:08 +1000

    Update 01-qc.sh

--- WGS/01-qc.sh
+++ WGS/01-qc.sh
@@ -7,51 +7,38 @@
 set -xeuo pipefail
 
 echo ------------------------------------------------------
-echo SBATCH: working directory is $SLURM_SUBMIT_DIR
-echo SBATCH: job identifier is $SLURM_JOBID
-echo SBATCH: array_ID is ${SLURM_ARRAY_TASK_ID}
+echo "SBATCH: working directory is $(pwd)"
+echo "SBATCH: job identifier is $SLURM_JOBID"
+echo "SBATCH: array_ID is ${SLURM_ARRAY_TASK_ID}"
 echo ------------------------------------------------------
-
-echo working dir is $PWD
-
-#cd into work dir
-echo changing to SLURM_SUBMIT_DIR
-cd "$SLURM_SUBMIT_DIR"
-echo working dir is now $PWD
 
 ########## Modules #################
 
 module load fastqc/0.11.9-java-11
 
-########## Set up dirs #################
+########## Set up directories #################
 
-#get job ID
-#use sed, -n supression pattern space, then 'p' to print item number {PBS_ARRAYID} eg 2 from {list}
+# Use 'sed' with -n option for suppressing pattern space and 'p' to print item number {PBS_ARRAYID} (e.g., 2 from {list})
 ID="$(/bin/sed -n ${SLURM_ARRAY_TASK_ID}p ${LIST})"
 
 fastqcfolder=analysis/fastqc_raw
 mkdir -p $fastqcfolder
 
-# check how many satqs there are - assumes "fastq" suffix
-fastqs="$(find ./reads -type f -name ${ID}*.fastq*)"
-# convert to array to count elements
+# Check how many fastq files there are - assumes "fastq" suffix
+fastqs="$(find ${FASTQ_DIR} -type f -name ${ID}.fastq)"
+
+# Convert to array to count elements
 fastqs_count=($fastqs)
 
-# check if single or paired end by looking for R2 file
+# Check if single or paired end by looking for the number of files that match the sample name
 if (( "${#fastqs_count[@]}" == 2 )); then
-
-echo "paired reads"
-
-########## Run #################
-fastqc -o $fastqcfolder reads/${ID}_R1*.fastq.gz reads/${ID}_R2*.fastq.gz
-
+  # Run FastQC on paired-end reads
+  fastqc -o $fastqcfolder ${FASTQ_DIR}/${ID}_R1*.fastq ${FASTQ_DIR}/${ID}_R2*.fastq
+  echo "Paired reads"  
 else
-echo "assuming single end"
-
-########## Run #################
-
-fastqc -o $fastqcfolder reads/${ID}_R1*.fastq.gz
-
+  # Run FastQC on single-end reads
+  fastqc -o $fastqcfolder ${FASTQ_DIR}/${ID}_R1*.fastq
+  echo "Assuming single end"
 fi
 
-echo Done QC now you should run multiqc in output directory to summarise
+echo "Done QC. Now you should run multiqc in the output directory to summarize."
